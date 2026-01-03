2818ec9a5bc02e5265772ec3ad07afb3196eb649
adamahmed29623-bit <adamahmed29623@gmail.com>
2026-01-03 16:28:22 +0200

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'royal-gold': '#d4af37',
+        'royal-blue': '#1e293b',
+      },
+    },
+  },
+  plugins: [],
+}
